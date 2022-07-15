597f731a374d697b70533713d32c9c7d59bb42eb
wfc <wfc@wfcmba.local>
2022-07-15 13:29:56 +0900

    fix install script

--- ruststartup.sh
+++ ruststartup.sh
@@ -2,4 +2,19 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+tmp=`echo $SHELL`
+sh=`echo ${tmp: -4:4}`
+
+if [ $sh == "bash" ]; then
+	source ~/.bashrc
+fi
+
+if [ $sh == "/zsh" ]; then
+	source ~/.zshrc
+fi
+
+if [ $sh == "fish" ]; then
+	source ~/.fishrc
+fi
+
 cargo install exa bat procs ripgrep fd-find hexyl tokei deno zellij
