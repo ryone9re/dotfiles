526aee773f663deacf4100c4f2f7d65852b21c64
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-01-08 09:16:17 +0900

    Update ruststartup.sh

--- ruststartup.sh
+++ ruststartup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-prog="exa bat procs ripgrep fd-find hexyl tokei zellij fnm"
+prog="exa bat fnm procs ripgrep fd-find hexyl tokei zellij"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source ~/.bashrc
 
 cargo install $prog
