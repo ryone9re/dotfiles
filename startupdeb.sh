bc5fe8bb38f44a3d0880f29b5732666946622350
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-01-08 09:21:43 +0900

    Update startupdeb.sh

--- startupdeb.sh
+++ startupdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prog="binutils build-essential clangd clang-format cmake curl diffutils lldb llvm neovim pkg-config"
+prog="binutils build-essential clangd clang-format cmake curl diffutils lldb llvm neovim pkg-config python3-pip"
 
 yes | sudo add-apt-repository ppa:neovim-ppa/unstable
 yes | sudo apt update
