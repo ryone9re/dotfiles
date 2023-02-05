e4dcae60af47105783de10fba9b9d1baa48ab111
ryone9re <ryoichi.kurimoto@gmail.com>
2023-02-06 02:59:00 +0900

    ghcup

--- ruststartup.sh
+++ ruststartup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prog="exa bat fnm procs ripgrep fd-find hexyl tokei zellij"
+prog="exa bat deno fnm procs ripgrep fd-find hexyl tokei zellij"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
