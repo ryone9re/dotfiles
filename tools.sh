aa4f18ddbf52c5c8a264f7a82e03b5370dccb0c0
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-03-01 02:53:57 +0900

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -6,3 +6,5 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install $prog -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
