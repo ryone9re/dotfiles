530beb13229b2352833c4ff0794eb4e7de5b5e43
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-03-01 02:51:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,53 +1,9 @@
-#!/bin/bash
-
-tmp=`echo $SHELL`
-sh=`echo ${tmp: -4:4}`
-
-if [ $sh == "bash" ]; then
-	shell="bash"
-	rc=".bashrc"
-fi
-
-if [ $sh == "/zsh" ]; then
-	shell="zsh"
-	rc=".zshrc"
-fi
-
-if [ $sh == "fish" ]; then
-	shell="fish"
-	rc=".fishrc"
-fi
-
+#!/bin/sh
 set -u
 
 for f in .??*; do
 	[ "$f" = ".git" ] && continue
 	[ "$f" = ".gitmodules" ] && continue
-
-	if [ "$f" = ".bashrc" ]; then
-		if [ "$shell" = "bash" ]; then
-			ln -snfv ${PWD}/"$f" ~/
-		else
-			continue
-		fi
-	fi
-
-	if [ "$f" = ".zshrc" ]; then
-		if [ "$shell" = "zsh" ]; then
-			ln -snfv ${PWD}/"$f" ~/
-		else
-			continue
-		fi
-	fi
-
-	if [ "$f" = ".fishrc" ]; then
-		if [ "$shell" = "fish" ]; then
-			ln -snfv ${PWD}/"$f" ~/
-		else
-			continue
-		fi
-	fi
-
 	ln -snfv ${PWD}/"$f" ~/
 done
 
