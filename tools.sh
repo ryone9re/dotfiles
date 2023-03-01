54ea5578a88c0b86cc7dc9d0b5d6fea4689aba3d
ryone9re <ryoichi.kurimoto@gmail.com>
2023-03-02 02:19:55 +0900

    to dracula

--- tools.sh
+++ tools.sh
@@ -2,7 +2,7 @@
 
 prog="binutils build-essential clangd clang-format cmake curl diffutils lldb llvm neovim pkg-config python3-pip zsh"
 
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install $prog -y
