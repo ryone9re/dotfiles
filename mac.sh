0f77048c441b542b3b2c1f7c0f0e90a0cb8175f6
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-03-26 14:37:24 +0900

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-prog="alacritty bat binutils clang-format cmake curl dbeaver-community diffutils discord deno exa fd fnm gcc git google-chrome google-japanese-ime hexyl jetbrains-toolbox karabiner-elements kicad llvm make neovim notion procs raycast ripgrep termius the-unarchiver tokei visual-studio-code zellij zoom"
+prog="alacritty bat binutils clang-format cmake curl dbeaver-community diffutils discord deno exa fd fnm gcc git google-chrome hexyl jetbrains-toolbox karabiner-elements kicad llvm make neovim notion procs raycast ripgrep termius the-unarchiver tokei visual-studio-code zellij zoom"
 
 brew install $prog
 
