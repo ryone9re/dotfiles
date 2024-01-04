2e552bd389a99ca7f8767b1708b5f8274a6f16eb
ryone9re <ryoichi.kurimoto@gmail.com>
2024-01-04 14:36:28 +0900

    remove

--- mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-prog="alacritty bat binutils clang-format cmake curl dbeaver-community diffutils discord deno eza fd fnm gcc git google-chrome hexyl jetbrains-toolbox karabiner-elements kicad llvm make neovim notion procs raycast ripgrep termius the-unarchiver tokei visual-studio-code zellij zoom zsh-completions"
-
-brew install $prog
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
--- rust.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-prog="eza bat deno fnm procs ripgrep fd-find hexyl tokei zellij"
-cargo install $prog
