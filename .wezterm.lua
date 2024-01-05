b83333ee29a76eb27ec477886d812715da3d8995
ryo <ryo@moon.local>
2024-01-05 18:54:40 +0900

    aaa

---
+++ .wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require "wezterm"
+
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Theme settings
+config.color_scheme = "Dracula (Official)"
+config.use_fancy_tab_bar = false
+
+-- Font settings
+config.font = wezterm.font("PlemolJP Console NF", {weight=450, stretch="Normal", style="Normal"})
+config.font_size = 16.0
+
+-- Key bindings
+config.keys = {
+  { key = "LeftArrow", mods = "SUPER", action = wezterm.action.ActivateTabRelative(-1) },
+  { key = "RightArrow", mods = "SUPER", action = wezterm.action.ActivateTabRelative(1) },
+
+  { key = "LeftArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "RightArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+  { key = "UpArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "DownArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+
+  { key = "v", mods = "ALT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "h", mods = "ALT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+
+  { key = "w", mods = "SUPER", action = wezterm.action.CloseCurrentPane { confirm = true } },
+  { key = "W", mods = "SUPER", action = wezterm.action.CloseCurrentTab { confirm = true } },
+}
+
+return config
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 ln -snfv ${PWD}/.zprofile ~/.zprofile
 ln -snfv ${PWD}/.zshrc ~/.zshrc
+ln -snfv ${PWD}/.wezterm.lua ~/.wezterm.lua
