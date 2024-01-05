b288c7b58322159a1f0208de0cd2eb1f9d760724
ryone9re <48243891+ryone9re@users.noreply.github.com>
2024-01-05 17:21:32 +0900

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-PROG="appcleaner notion orbstack raycast rectangle karabiner-elements discord slack google-japanese-ime the-unarchiver visual-studio-code wezterm"
+PROG="appcleaner notion orbstack raycast rectangle karabiner-elements discord slack the-unarchiver visual-studio-code wezterm"
 RUST_TOOLS="eza bat procs ripgrep fd hexyl tokei"
 
 brew install $PROG $RUST_TOOLS
