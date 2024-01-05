018ba4e31473b3d6103064a69aa1da2b228eb148
ryone9re <48243891+ryone9re@users.noreply.github.com>
2024-01-05 17:12:45 +0900

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-PROG="appcleaner google-chrome notion orbstack raycast rectangle karabiner-elements discord slack google-japanese-ime the-unarchiver visual-studio-code wezterm"
+PROG="appcleaner notion orbstack raycast rectangle karabiner-elements discord slack google-japanese-ime the-unarchiver visual-studio-code wezterm"
 RUST_TOOLS="eza bat procs ripgrep fd-find hexyl tokei"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
