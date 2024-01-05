753f2158ad997d041ef681eb1b8e9046144ff531
ryone9re <48243891+ryone9re@users.noreply.github.com>
2024-01-05 17:13:01 +0900

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,6 +3,4 @@
 PROG="appcleaner notion orbstack raycast rectangle karabiner-elements discord slack google-japanese-ime the-unarchiver visual-studio-code wezterm"
 RUST_TOOLS="eza bat procs ripgrep fd-find hexyl tokei"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 brew install $PROG $RUST_TOOLS
