f17c5cb8807681ab2700a7cde28f8f8764692b23
ryone9re <ryo@moon.local>
2024-01-23 15:18:18 +0900

    commit

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 ln -snfv ${PWD}/.zprofile ~/.zprofile
 ln -snfv ${PWD}/.zshrc ~/.zshrc
 ln -snfv ${PWD}/.wezterm.lua ~/.wezterm.lua
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
