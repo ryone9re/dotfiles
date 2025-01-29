ba49e2f3f93eafaff7b6664472cd4549d9177df4
ryone9re <ryoichi.kurimoto@gmail.com>
2025-01-29 18:21:42 +0900

    A

--- packages.sh
+++ packages.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -eu
 
 BROWSER="google-chrome"
-PROG="appcleaner discord google-japanese-ime karabiner-elements monitorcontrol notion orbstack raycast rectangle slack stack the-unarchiver visual-studio-code wezterm"
+PROG="appcleaner discord karabiner-elements monitorcontrol notion orbstack raycast rectangle slack stack the-unarchiver visual-studio-code wezterm"
 RUST_TOOLS="bat eza fd hexyl procs tokei ripgrep"
 
 brew install $BROWSER $PROG $RUST_TOOLS
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+brew install google-japanese-ime 
