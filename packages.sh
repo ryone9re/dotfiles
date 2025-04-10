8d65e8c9c529f9d9bd745229efe99c2e0b688fbe
ryone9re <ryoichi.kurimoto@gmail.com>
2025-04-10 12:50:10 +0900

    eikana

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 
 BROWSER="google-chrome"
-PROG="appcleaner discord ghcup google-japanese-ime karabiner-elements monitorcontrol notion orbstack raycast rectangle slack the-unarchiver visual-studio-code wezterm"
+PROG="appcleaner cmd-eikana discord ghcup google-japanese-ime monitorcontrol notion orbstack raycast rectangle slack the-unarchiver visual-studio-code wezterm"
 RUST_TOOLS="bat eza fd hexyl procs tokei ripgrep"
 
 sudo softwareupdate --install-rosetta
