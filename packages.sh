5f8db45dc8114ce63240934c729fa15470fd41d5
ryone9re <ryoichi.kurimoto@gmail.com>
2025-08-15 14:57:09 +0900

    Add packages

--- packages.sh
+++ packages.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eu
 
 BROWSER="google-chrome"
-PROG="appcleaner cmd-eikana discord google-japanese-ime mise monitorcontrol notion orbstack raycast rectangle slack the-unarchiver visual-studio-code wezterm"
-RUST_TOOLS="bat eza fd hexyl procs tokei ripgrep"
+PROG="appcleaner cmd-eikana discord google-japanese-ime k9s mise monitorcontrol notion orbstack raycast rectangle slack the-unarchiver visual-studio-code wezterm"
+RUST_TOOLS="bat difftastic eza fd hexyl procs tokei ripgrep"
 
 brew install $BROWSER $PROG $RUST_TOOLS
 mise install
